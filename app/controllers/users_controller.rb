26f38350458b7d81380092704b568731422691c3
bryant tee <bryantt23@gmail.com>
2015-01-25 22:33:53 -0800

    bb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+  @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+  if @user.save
+    redirect_to new_user_path
+  else
+    render :new
   end
+end
+
+
+
+# helper method
+private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
