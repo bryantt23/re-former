569debeee75f13dd992c0824b7612b20f65973e3
bryant tee <bryantt23@gmail.com>
2015-01-25 23:16:38 -0800

    4d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
   protect_from_forgery 
   
   def new
+#     instance variable = instantiate a blank User object
+    @user = User.new
   end
 
   def create
 #     accept normal top level User attributes
-    @user = User.new(:username => params[:username], 
-    :email => params[:email], :password => params[:password])
+    # @user = User.new(:username => params[:username], 
+    # :email => params[:email], :password => params[:password])
+   
+    
+#       accept the nested :user hash from params
+    @user = User.new(user_params)      
       if @user.save
         redirect_to new_user_path
       else
